8e7585b6325a6f5e32c80d7b86c798e71ebfe6cb
Vladislav <kentuukq@gmail.com>
2024-04-22 02:49:38 +0300

    Отображение выбранного цвета

--- app/src/main/java/com/topic2/android/notes/ui/componets/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/componets/screens/SaveNoteScreen.kt
@@ -128,6 +128,7 @@ fun ColorItem(
 @Preview @Composable
 fun ColorItemPreview() { ColorItem(ColorModel.DEFAULT) {} }
 fun SaveNoteScreen(viewModel: MainViewModel) {
+
 @Composable
 private fun SaveNoteTopAppBar(
     isEditingMode: Boolean,
