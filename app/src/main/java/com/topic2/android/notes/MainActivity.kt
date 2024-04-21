f4bf98b8c97538a5c7cd1be25bd28e041e81c7b1
Vladislav <kentuukq@gmail.com>
2024-04-22 02:22:35 +0300

    Добавление MainActivityScreen

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.NotesRouter
 import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
 import com.topic2.android.notes.ui.componets.AppDrawer
 import com.topic2.android.notes.ui.componets.Note
 import com.topic2.android.notes.ui.componets.screens.NotesScreen
+import com.topic2.android.notes.ui.componets.screens.SaveNoteScreen
 import com.topic2.android.notes.viewmodel.MainViewModel
+import com.topic2.android.notes.ui.componets.screens.TrashScreen
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import kotlinx.coroutines.launch
 
@@ -36,6 +42,13 @@ class MainActivity : AppCompatActivity() {
       NotesTheme {
         NotesScreen(viewModel = viewModel)
       }
+@Composable @ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel) {
+  Surface {
+    when (NotesRouter.currentScreen) {
+      is Screen.Notes -> NotesScreen(viewModel)
+      is Screen.SaveNote -> SaveNoteScreen(viewModel)
+      is Screen.Trash -> TrashScreen(viewModel)
     }
   }
 }
